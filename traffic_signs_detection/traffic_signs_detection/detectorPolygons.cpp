465ccc66a2bf502d262ba992a3fe6509038eb56e
rongirl <gro.pretty.arabella@mail.ru>
2023-03-13 17:52:27 +0300

    Added highlightColor()

--- traffic_signs_detection/traffic_signs_detection/detectorPolygons.cpp
+++ traffic_signs_detection/traffic_signs_detection/detectorPolygons.cpp
@@ -6,7 +6,7 @@ std::vector<cv::Vec3f> DetectorPolygons::detectCircle(const cv::Mat &frame)
 {
     cv::Mat grayImage;
     cvtColor(frame, grayImage, cv::COLOR_BGR2GRAY);
-    medianBlur(grayImage, grayImage, 5);
+  //  medianBlur(grayImage, grayImage, 5);
     std::vector<cv::Vec3f> circles;
     cv::HoughCircles(grayImage, circles, cv::HOUGH_GRADIENT, 1,
         grayImage.rows / 16,  
