d757419b1b2e895e41eed5d69e97b426e189154e
illided <utkin.ilya.nickolaevich@gmail.com>
2023-04-04 14:26:46 +0300

    added hyperopt backend

--- DecisionTrees/core/__init__.py
+++ DecisionTrees/core/__init__.py
@@ -2,3 +2,4 @@ from .lib.environment import Environment
 from .lib.description import ParametersDescription
 from .lib.learners.reinforce import REINFORCELearner
 from .lib.learners.genetic import GeneticLearner
+from .lib.learners.hyperopt import HyperOptLearner
